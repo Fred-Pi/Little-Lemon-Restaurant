db4e98e71991d21f40bb0a42021bd25e3c3dcc26
Fred-Pi <147168792+Fred-Pi@users.noreply.github.com>
2024-04-22 17:15:22 +0200

    OurStory component Fix. Text and Pictures added

--- src/components/pages/Home/Hero.js
+++ src/components/pages/Home/Hero.js
@@ -1,31 +1,31 @@
-import { Link } from 'react-router-dom';
+import {Link} from 'react-router-dom';
 import restaurantFoodImage from './assets/restaurant-food.jpg';
 import './Hero.css';
 import pages from '../../../utils/pages';
 
 const Hero = () => {
-  return (
-    <section className="hero">
-      <div className="container grid">
-        <div className="hero-information">
-          <h1>Little Lemon</h1>
-          <h2>Chicago</h2>
-          <p>
-            We are a family owned Mediterranean restaurant, 
-            focused on traditional recipes served with a modern twist.
-          </p>
-          <Link className="button-primary" to={pages.get('bookings').path}>
-            Reserve a table
-          </Link>
-        </div>
-        <img 
-          className="hero-image" 
-          src={restaurantFoodImage} 
-          alt="Restaurant food" 
-        />
-      </div>
-    </section>
-  );
+    return (
+        <section className="hero">
+            <div className="container grid">
+                <div className="hero-information">
+                    <h1>Little Lemon</h1>
+                    <h2>Chicago</h2>
+                    <p>
+                        Little Lemon is a Mediterranean inspired bar and small plates restaurant focusing on fresh
+                        produce and big flavours, offering bespoke cocktails and a premium wine selection.
+                    </p>
+                    <Link className="button-primary" to={pages.get('bookings').path}>
+                        Reserve a table
+                    </Link>
+                </div>
+                <img
+                    className="hero-image"
+                    src={restaurantFoodImage}
+                    alt="Restaurant food"
+                />
+            </div>
+        </section>
+    );
 };
 
 export default Hero;
--- src/components/pages/Home/OurStory.js
+++ src/components/pages/Home/OurStory.js
@@ -1,35 +1,32 @@
 import chefsMarioAndAdrianAImage from './assets/chefs-mario-and-adrian_a.jpg';
-import chefsMarioAndAdrianBImage from './assets/chefs-mario-and-adrian_b.jpg';
+import chefsMarioAndAdrianBImage from './assets/nick-karvounis-Ciqxn7FE4vE-unsplash.jpg';
 import './OurStory.css';
 
 const OurStory = () => {
-  return (
-    <section className="container grid our-story">
-      <div className="our-story-description">
-        <h1>Little Lemon</h1>
-        <h2>Chicago</h2>
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do 
-          eiusmod tempor incididunt ut labore et dolore magna aliqua. 
-          Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris 
-          nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in 
-          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla 
-          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in 
-          culpa qui officia deserunt mollit anim id est laborum. Sed ut 
-          perspiciatis unde omnis iste natus error sit voluptatem accusantium 
-          doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo 
-          inventore veritatis et quasi architecto beatae vitae dicta sunt 
-          explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut 
-          odit aut fugit, sed quia consequuntur magni dolores eos qui ratione 
-          voluptatem sequi nesciunt.
-        </p>
-      </div>
-      <div className="our-story-chefs">
-        <img src={chefsMarioAndAdrianAImage} alt="Chefs Mario and Adrian" />
-        <img src={chefsMarioAndAdrianBImage} alt="Chefs Mario and Adrian" />
-      </div>
-    </section>
-  );
+    return (
+        <section className="container grid our-story">
+            <div className="our-story-description">
+                <h1>Little Lemon</h1>
+                <h2>Chicago</h2>
+                <p className="text-center">
+                    Little Lemon isn't just a captivating Mediterranean cocktail and wine bar—it's also the perfect
+                    venue to host your next event. Whether you're planning a small gathering or a private affair, we
+                    have you covered.
+                    <br></br>
+                    For intimate gatherings, you can easily book online to reserve space for your small group. Enjoy our
+                    upscale Mediterranean finger food platters, carefully crafted to impress and satisfy your guests'
+                    palates. If you're looking to host a larger event or wish to book the entire venue exclusively, we
+                    welcome you to fill out our booking enquiry form. Our dedicated events team will work closely with
+                    you to tailor a package that suits your specific requirements. From corporate events to private
+                    celebrations, we have the expertise and flexibility to make your event a resounding success.
+                </p>
+            </div>
+            <div className="our-story-chefs">
+                <img src={chefsMarioAndAdrianAImage} alt="Chefs Mario and Adrian"/>
+                <img src={chefsMarioAndAdrianBImage} alt="Chefs Mario and Adrian"/>
+            </div>
+        </section>
+    );
 };
 
 export default OurStory;
